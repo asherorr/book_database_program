b03e16945384e97b8ac6e1acfba850c88a5ea877
asherorr <asher@mailfence.com>
2022-08-27 16:24:42 -0400

    This commit will create the initial commit!

---
+++ app.py
@@ -0,0 +1,9 @@
+# import models
+# main menu (add, search, analysis, exit, view options)
+# add books to the database
+# edit books
+# delete them
+# search function
+# data cleaning function
+# loop to run the program
+# when the user decides to exit, the program stops.
---
+++ models.py
@@ -0,0 +1,4 @@
+# create a database
+# give it a name (books.db)
+# create a model (Title, Author, Date Published, and Price.)
+
