5a5384362a513ec694945922923b5c77c17a38c9
asherorr <asher@mailfence.com>
2022-08-29 15:07:31 -0400

    This commit will add the ability to add books in the db.

--- app.py
+++ app.py
@@ -105,8 +105,10 @@ def app():
             print('Book added!"')
             time.sleep(1.5)
         elif choice == "2":
-            # view books
-            pass
+            # view bookS
+            for book in session.query(Book):
+                print(f'{book.id} | {book.title} | {book.author}')
+            input('\nPress enter to return to the main menu.')
         elif choice == "3":
             #search for book
             pass
