5dedab23746e904e3a078ceeb23c09d3bbd65b4f
asherorr <asher@mailfence.com>
2022-08-29 21:38:37 -0400

    This commit will make it possible to view books.

--- app.py
+++ app.py
@@ -59,7 +59,27 @@ def clean_price(price_str):
               \r*****''')
     else:
         return int(price_float * 100)
-        
+    
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        input(f''''
+              \n***** ID ERROR *****
+              \rThe ID should be a number.
+              \rPress enter to try again.
+              \r*****''')
+        return
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input(f''''
+              \n***** ID ERROR *****
+              \rOptions: {options}
+              \rPress enter to try again.
+              \r*****''')
+            return
     
     
 # when the user decides to exit, the program stops.
@@ -105,13 +125,27 @@ def app():
             print('Book added!"')
             time.sleep(1.5)
         elif choice == "2":
-            # view bookS
             for book in session.query(Book):
                 print(f'{book.id} | {book.title} | {book.author}')
             input('\nPress enter to return to the main menu.')
         elif choice == "3":
-            #search for book
-            pass
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input(f'''
+                    \nID Options: {id_options}
+                    \rBook id: ''')
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f'''
+                  \n{the_book.title} by {the_book.author}
+                  \rPublished: {the_book.published_date}
+                  \rPrice: ${the_book.price / 100}''')
+            input('\nPress enter to return to the main menu.')
         elif choice == "4":
             #book analysis
             pass
